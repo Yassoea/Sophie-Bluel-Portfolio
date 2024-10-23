157efaf47d2d9e1dc6a26a56ffa534e912ad8390
Yassoea <elakhalyassine10@gmail.com>
2024-10-23 18:03:48 +0100

    mode edition done

--- FrontEnd/JavaScript/app.js
+++ FrontEnd/JavaScript/app.js
@@ -2,6 +2,7 @@ const url = "http://localhost:5678/api";
 
 getWorks();
 getCategories();
+displayAdminMode();
 
 
 
@@ -99,3 +100,21 @@ function setFilter(data) {
   }
   
   document.querySelector(".tous").addEventListener("click", () => getWorks());
+
+  // Utilisateur authentifié
+function displayAdminMode() {
+  if (sessionStorage.authToken) {
+    // document.querySelector(".div-container").style.display = "none";
+    // document.querySelector(".js-modal-2").style.display = "block";
+    document.querySelector(".gallery").style.margin = "30px 0 0 0";
+    const editBanner = document.createElement("div");
+    editBanner.className = "edit"; 
+    editBanner.innerHTML =
+      '<p><a href="#modal1" class="js-modal"><i class="fa-regular fa-pen-to-square"></i>Mode édition</a></p>';  
+        document.body.prepend(editBanner);
+    document.querySelector(".log-button").textContent = "logout";
+    document.querySelector(".log-button").addEventListener("click", () => {
+      sessionStorage.removeItem("authToken");
+    });
+  }
+}
